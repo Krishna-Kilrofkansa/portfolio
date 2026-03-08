eb88d3b724c353598fbea4c9651f4a98ae7d1881
Krishna-Kilrofkansa <aakarshakswaraj2004@gmail.com>
2026-03-08 23:20:16 +0530

    new file

--- components/ResumeSection.js
+++ components/ResumeSection.js
@@ -23,7 +23,7 @@ export default function ResumeSection() {
                         <path d="m19 12-7 7-7-7"></path>
                     </svg>
                 </a>
-                <a href="/a1.ipynb" className="view-btn" download onClick={handleDownload}>
+                <a href="/cognitive.ipynb" className="view-btn" download onClick={handleDownload}>
                     Download Resume
                     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
                         <path d="M12 5v14"></path>
